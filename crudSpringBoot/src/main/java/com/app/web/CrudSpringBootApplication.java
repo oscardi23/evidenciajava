9b5589bd1b91721b98447ae275291fc2d641ffc2
oscardi23 <142261882+oscardi23@users.noreply.github.com>
2023-10-02 22:16:01 -0500

    Add files via upload

---
+++ crudSpringBoot/src/main/java/com/app/web/CrudSpringBootApplication.java
@@ -0,0 +1,34 @@
+package com.app.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.app.web.entidad.Estudiante;
+import com.app.web.repositorio.EstudianteRepositorio;
+
+@SpringBootApplication
+public class CrudSpringBootApplication implements CommandLineRunner  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootApplication.class, args);
+	}
+
+	
+	@Autowired
+	private EstudianteRepositorio repositorio;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		/*
+		 Estudiante estudiante1 = new Estudiante("oscar","diaz","oscar@gmail.com" );
+		 repositorio.save(estudiante1);
+		 
+		 Estudiante estudiante2 = new Estudiante("kathe","ruiz","kathe@gmail.com" );
+		 repositorio.save(estudiante2);
+	*/
+	}
+
+}
